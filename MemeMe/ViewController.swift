373236bf4571dcbfdb07f8d1e9430e1d6e0ee1e9
oneway101 <oneway.first@gmail.com>
2016-11-01 14:24:58 -0400

    MemeMe v1.0 - picks an image from Photo Album and disable camera button

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ha Na Gill on 10/31/16.
+//  Copyright © 2016 Ha Na Gill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imagePickerView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func pickImageFromAlbum(sender: AnyObject){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func pickImageFromCamera(sender:AnyObject){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+
+}
